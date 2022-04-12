1c67d479fbdfb3ed1d4a55cd76814543761c549e
almrttsn <alimurat_tasin@hotmail.com>
2022-04-12 15:07:51 +0300

    de-activate spheres process implemented

--- Assets/TestCubes.cs
+++ Assets/TestCubes.cs
@@ -7,7 +7,7 @@ public class TestCubes : MonoBehaviour
     [SerializeField] GameManager _gameManager;
     [SerializeField] GameObject[] _cubes; //küpleri array içine aldýk.
     private float _waitTimer = 1f;
-    public bool ActivateOrderCame;
+    public bool ActivateCubesOrderCame;
 
 
     private void Start()
@@ -21,21 +21,27 @@ public class TestCubes : MonoBehaviour
     {
         for (int i = 0; i < _cubes.Length; i++)
         {
-            if (_cubes[i].activeInHierarchy == true)
+            if (_cubes[i].activeInHierarchy == true)    //1'er saniye aralýklarla küpleri deaktif ettik.
             {
                 _cubes[i].SetActive(false);
                 yield return new WaitForSeconds(_waitTimer); 
             }
         }
-        ActivateCubes();
+        ActivateCubes();    //küpleri tekrar aktif ettik.
+        DeactiveSpheresOrderSent(); //küreleri deaktif etme emrini gönder.
     }
 
-    public void ActivateCubes()
+    public void ActivateCubes() //aktive etme yöntemi.
     {
-        if(ActivateOrderCame == true)
+        if(ActivateCubesOrderCame == true) //Game Manager'dan emir geldi. Emri veren TestSpheres
             for (int i = 0; i<_cubes.Length; i++)
             {
                 _cubes[i].SetActive(true);
             }
+    }
+
+    public void DeactiveSpheresOrderSent() //emirin içeriði.
+    {
+        _gameManager.TestSpheres.DeactivateSpheresOrderSent = true;
     }
 }
--- Assets/TestSpheres.cs
+++ Assets/TestSpheres.cs
@@ -7,6 +7,7 @@ public class TestSpheres : MonoBehaviour
     [SerializeField] GameManager _gameManager;
     [SerializeField] List<GameObject> _spheres; //küreleri list içine aldýk.
     private float _senderTimeToWait = 1f;
+    public bool DeactivateSpheresOrderSent;
 
     private void Start()
     {
@@ -17,18 +18,26 @@ public class TestSpheres : MonoBehaviour
 
     private IEnumerator MoveSpheresForward()
     {
-        for (int i = 0; i < _spheres.Count; i++)
+        for (int i = 0; i < _spheres.Count; i++) //1'er saniye aralýklarla küreleri ileri taþýdýk.
         {
             _spheres[i].transform.position = _spheres[i].transform.position + new Vector3(0, 0, 5f);
             yield return new WaitForSeconds(_senderTimeToWait);
         }
-        ActivateOrderSend();
+        ActivateCubesOrderSent(); //son gönderme iþlemi bittikten sonra isteði gönderdik.
+        DeactivateSpheres(); //küreleri burada deaktif et.
     }
 
-    public void ActivateOrderSend()
+    public void ActivateCubesOrderSent()
     {
-        _gameManager.TestCubes.ActivateOrderCame = true;
+        _gameManager.TestCubes.ActivateCubesOrderCame = true; //istek doðrultusunda ilgili bool'u true gönderme emri verdik.
     }
 
+    public void DeactivateSpheres() //küreleri deaktif etme yöntemi.
+    {
+        for (int i = 0; i<_spheres.Count; i++)
+        {
+            _spheres[i].SetActive(false);
+        }
+    }
 
 }
