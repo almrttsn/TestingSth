b7ada4f8287ef23750b0efc504ead3c374d3fdcb
almrttsn <alimurat_tasin@hotmail.com>
2022-04-12 12:10:38 +0300

    script tests OK

--- Assets/TestCubes.cs
+++ Assets/TestCubes.cs
@@ -4,25 +4,25 @@ using UnityEngine;
 
 public class TestCubes : MonoBehaviour
 {
-    [SerializeField] GameObject[] cubes; //küpleri array içine aldýk.
-    private float waitTimer = 1f;
+    [SerializeField] GameObject[] _cubes; //küpleri array içine aldýk.
+    private float _waitTimer = 1f;
 
 
     private void Start()
     {
         Debug.Log("Trying to find specific cubes");
-        Debug.Log(cubes[2].name.ToString()); //istediðimiz sýradaki objenin ismini yazdýrdýk.
-        StartCoroutine(StartCubesDeactivateProcess());
+        Debug.Log(_cubes[2].name.ToString()); //istediðimiz sýradaki küpün ismini yazdýrdýk.
+        StartCoroutine(StartCubesDeactivateProcess()); //1'er saniye aralýklarla küpleri deaktif ettik.
     }
 
     private IEnumerator StartCubesDeactivateProcess()
     {
-        for (int i = 0; i < cubes.Length; i++)
+        for (int i = 0; i < _cubes.Length; i++)
         {
-            if (cubes[i].activeInHierarchy == true)
+            if (_cubes[i].activeInHierarchy == true)
             {
-                cubes[i].SetActive(false);
-                yield return new WaitForSeconds(waitTimer); //1'er saniye aralýklarla objeleri deaktif ettik.
+                _cubes[i].SetActive(false);
+                yield return new WaitForSeconds(_waitTimer); 
             }
         }
 
--- Assets/TestSphere.cs
+++ Assets/TestSphere.cs
@@ -4,11 +4,23 @@ using UnityEngine;
 
 public class TestSphere : MonoBehaviour
 {
-    [SerializeField] List<GameObject> spheres; //küreleri list içine aldýk.
+    [SerializeField] List<GameObject> _spheres; //küreleri list içine aldýk.
+    private float _senderTimeToWait = 1f;
 
     private void Start()
     {
-        
+        Debug.Log("Try to find capacity of list and move objects one by one");
+        Debug.Log(_spheres.Count); //listenin kapasitesini yazdýrdýk.
+        StartCoroutine(MoveSpheresForward()); //1'er saniye aralýklarla küreleri ileri taþýdýk.
+    }
+
+    private IEnumerator MoveSpheresForward()
+    {
+        for (int i = 0; i < _spheres.Count; i++)
+        {
+            _spheres[i].transform.position = _spheres[i].transform.position + new Vector3(0, 0, 5f);
+            yield return new WaitForSeconds(_senderTimeToWait);
+        }
     }
 
 
